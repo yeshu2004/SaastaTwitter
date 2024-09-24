dbb04b198e61f0996eb945c16c65c65af3584962
Yeshu Agarwal <133958652+yeshu2004@users.noreply.github.com>
2024-09-25 01:28:03 +0530

    Update user.models.js

--- models/user.models.js
+++ models/user.models.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/mini-porject");
+mongoose.connect("process.env.MOONGODB_URL");
 
 const UserSchema = mongoose.Schema({
     name:String,
